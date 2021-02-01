1bf34e78cbfc91af891a1756c50b4f296b0614ac
nfwilmore42 <nfwilmore42@tntech.edu>
2021-02-01 11:17:35 -0600

    Commit

--- sankey.js
+++ sankey.js
@@ -1,51 +1,84 @@
  var mytitle;
+ var color = "white";
  var numNode =1;
  var numLink =1;
- var Label = ["outlet","fan","computer","waste"];
- var Source = [];
+ var myLabel = [];
+ var numSource = [];
  var dest = [];
  var linkVal= [];
+ var nodeColor= [];
+ var linkColor = [];
 
- function setTitle()
+ function newTitle()
 {
 	mytitle = document.getElementById("title").value;
 }
 
-function setLabel(numN)
+function chartColor()
 {
-	Label[numN]=document.getElementById("label"+String(numN)).value;
+	color = document.getElementById("colorPicker").value;
 }
 
-function setSource(numL)
+function newLabel(numN)
 {
-	Source[numL]=document.getElementById("source"+String(numL)).value;
+	myLabel[numN]=document.getElementById("label"+String(numN)).value;
+}
+function newNodeColor(numN)
+{
+	nodeColor[numN]=document.getElementById("nodeColor"+String(numN)).value;
 }
 
-function setDest(numL)
+function newLinkColor(numL)
+{
+	linkColor[numL]=document.getElementById("linkColor"+String(numL)).value;
+}
+
+function newSource(numL)
+{
+	numSource[numL]=document.getElementById("source"+String(numL)).value;
+}
+
+function newDest(numL)
 {
 	dest[numL]=document.getElementById("dest"+String(numL)).value;
 }	
 
-function setLinkVal(numL)
+function newLinkVal(numL)
 {
 	linkVal[numL]=document.getElementById("linkVal"+String(numL)).value;
 }
 
-function addInput()
+function deleteLink(numL)
+{
+	document.getElementById("Link"+numL).parentNode.removeChild(document.getElementById("Link"+numL));
+	numSource[numL]=null;
+	dest[numL]=null;
+	linkVal[numL]=null;
+}
+
+function deleteINode(numN)
+{
+	document.getElementById("Node"+numN).parentNode.removeChild(document.getElementById("Node"+numN));
+	myLabel[numN]= null;
+}
+
+function addNode()
 {
     var makeInputs = document.getElementById("makeInputs");
 
-    var newInput = document.createElement("td");
-    newInput.innerHTML = "<div class='col-sm-2'>"+
+	var newNode = document.createElement("td");
+	newNode.setAttribute("id", "Node"+numNode);
+    newNode.innerHTML = "<div class='col-sm-2' id='node"+numNode+">"+
 			"<div style='width: 225px;  display: inline-block;'>"+
 				"<table class='table table-bordered' style='margin: 0px;'>"+
 					"<tbody>"+
 						"<tr id='inputs'>"+
 							"<td style='width:120px' class='text-center'>"+
+								"<DIV align='right'> <button class='btn btn-secondary' onclick='deleteINode("+numNode+")' style='background-color: #8f3236'><span class='glyphicon glyphicon-remove'></span></button></DIV>" +
 								"<h3 style='margin-top: 5px'>Node "+numNode+"</h3>"+
 								"<div class='input-group'>"+
 									"<span class='input-group-addon'>Name</span>"+
-									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='setLabel("+numNode+")' style='width: 145px' id='label"+String(numNode)+"'>"+
+									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='newLabel("+numNode+")' style='width: 145px' id='label"+String(numNode)+"'>"+
 								"</div>"+
 								"<div class='input-group'>"+
 									"<span class='input-group-addon' id='basic-addon1'>Value</span>"+
@@ -55,18 +88,7 @@ function addInput()
 									"<span class='input-group-addon' id='basic-addon1'>Units</span>"+
 									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='updateDifference()' style='width: 151px'>"+
 								"</div>"+
-								"<div class='form-group' style='width: 208px'>"+
-									"<select class='form-control' id='sel1'>"+
-									  "<option>Red</option>"+
-									  "<option>Blue</option>"+
-									  "<option>Yellow</option>"+
-									  "<option>Orange</option>"+
-									  "<option>Green</option>"+
-									  "<option>Blue</option>"+
-									  "<option>Purple</option>"+
-									  "<option>Brown</option>"+
-									"</select>"+
-								  "</div>"+	
+									"<input type='color' class='inp' id='nodeColor"+numNode+"' onchange = 'newNodeColor("+numNode+")' style='width: 206px'>"+
 							"</td>"+
 						"</tr>"+
 					"</tbody>"+
@@ -75,7 +97,7 @@ function addInput()
 		"</div>";
 	
 	
-    makeInputs.appendChild(newInput);
+    makeInputs.appendChild(newNode);
     numNode++;
 }
 
@@ -83,42 +105,33 @@ function addLink()
 {
     var makeLinks = document.getElementById("makeLinks");
 
-    var newLink = document.createElement("td");
-    newLink.innerHTML = "<div class='col-sm-2'>"+
+	var newLink = document.createElement("td");
+	newLink.setAttribute("id", "Link"+numLink);
+    newLink.innerHTML = "<div class='col-sm-2' id='link"+numLink+"'>"+
 			"<div style='width: 275px;  display: inline-block;'>"+
 				"<table class='table table-bordered' style='margin: 0px;'>"+
 					"<tbody>"+
 						"<tr id='inputs'>"+
 							"<td style='width:120px' class='text-center'>"+
+								"<DIV align='right'> <button class='btn btn-secondary' onclick='deleteLink("+numLink+")' style='background-color: #8f3236'><span class='glyphicon glyphicon-remove'></span></button></DIV>" +
 								"<h3 style='margin-top: 5px'>Link "+numLink+"</h3>"+
 								"<div class='input-group'>"+
 									"<span class='input-group-addon'>Source #</span>"+
-									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='setSource("+numLink+")' style='width: 185px' id='source"+String(numLink)+"'>"+
+									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='newSource("+numLink+")' style='width: 185px' id='source"+String(numLink)+"'>"+
 								"</div>"+
 								"<div class='input-group'>"+
 									"<span class='input-group-addon'>Destination #</span>"+
-									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='setDest("+numLink+")' style='width: 160px' id='dest"+String(numLink)+"'>"+
+									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='newDest("+numLink+")' style='width: 160px' id='dest"+String(numLink)+"'>"+
 								"</div>"+
 								"<div class='input-group'>"+
 									"<span class='input-group-addon'>Value</span>"+
-									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='setLinkVal("+numLink+")' style='width: 207px'id='linkVal"+String(numLink)+"'>"+
+									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='newLinkVal("+numLink+")' style='width: 207px'id='linkVal"+String(numLink)+"'>"+
 								"</div>"+
 								"<div class='input-group'>"+
 									"<span class='input-group-addon' id='basic-addon1'>Units</span>"+
 									"<input type='text' class='form-control' aria-describedby='basic-addon1' onchange='updateDifference()' style='width: 210px'>"+
 								"</div>"+
-								"<div class='form-group' style='width: 267px'>"+
-									"<select class='form-control' id='sel1'>"+
-									  "<option>Red</option>"+
-									  "<option>Blue</option>"+
-									  "<option>Yellow</option>"+
-									  "<option>Orange</option>"+
-									  "<option>Green</option>"+
-									  "<option>Blue</option>"+
-									  "<option>Purple</option>"+
-									  "<option>Brown</option>"+
-									"</select>"+
-								  "</div>"+
+								"<input type='color' class='inp' id='linkColor"+numLink+"' onchange = 'newLinkColor("+numLink+")' style='width: 265px'>"+
 							"</td>"+
 						"</tr>"+
 					"</tbody>"+
@@ -131,23 +144,49 @@ function addLink()
     numLink++;
 }
 
-function makeSankey()
+function buildSvgArrows() 
+{
+    const rects = g.querySelectorAll('.node-rect');
+    const arrowOpacity = '0.9';
+    const arrowShape = 'polygon(100% 50%, 0 0, 0 100%)'
+
+	for (let i = 0; i < rects.length; i++) 
+	{
+	  if (!g.connectingNodes.includes(i)) 
+	  {
+        const height = rects[i].getAttribute('height');
+        const defaultY = rects[i].getAttribute('y');
+
+        rects[i].setAttribute('y', `${defaultY - (height / 2.75)}`);
+        rects[i].setAttribute('style', `width: ${height}px; height: ${height * 1.75}px; clip-path:  ${arrowShape}; 
+         stroke-width: 0.5; stroke: rgb(255, 255, 255); stroke-opacity: 0.5; fill-opacity: ${arrowOpacity};`);
+      }
+	}
+}
+
+function renderSankey()
 {
     var data = [{
     type: "sankey",
         arrangement: "snap",
         node:{
-            label: Label, //["A", "B", "C", "D", "E", "F"],
+			label: myLabel,
+			color: nodeColor,
            // x: [0.2, 0.2, 0.5, 0.7, 0.3, 0.5],
            // y: [0.7, 0.5, 0.2, 0.4, 0.2, 0.3],
             pad:10}, // 10 Pixels
         link: {
-            source: Source,
+            source: numSource,
             target: dest,
-            value: linkVal}
+			value: linkVal,
+			color: linkColor}
         }]
 
-    var layout = {"title": mytitle}
+    var layout = {
+		"title": mytitle,
+		paper_bgcolor: color
+	}
 
-    Plotly.newPlot('myDiv', data, layout)
+	Plotly.newPlot('myDiv', data, layout)
+	//buildSvgArrows();
 }
